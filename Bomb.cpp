351e53e3c2d6fac84e8c9b5edad0d8c1bb580681
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-02-14 18:37:01 +0300

    Update Bomb.cpp

--- Bomb.cpp
+++ Bomb.cpp
@@ -1,14 +1,33 @@
-#include <iostream>
-
 #include "Bomb.h"
-#include "MyTools.h"
+#include <iostream>
+#include "ScreenSingleton.h"
 
-using namespace std;
-using namespace MyTools;
-
-void Bomb::Draw() const
+void Bomb::Draw() const 
 {
-    MyTools::SetColor(CC_LightMagenta);
-    GotoXY(x, y);
-    cout << "*";
+//ScreenSingleton::getInstance().SetColor(CC_LightMagenta);
+  ScreenSingleton::getInstance().GotoXY(x, y);
+  std::cout << "*";
+}
+
+void BombDecorator::Move(uint16_t time)
+{
+    m_bomb->Move(time * 1.6);
+}
+
+void BombDecorator::Draw() const
+{
+    m_bomb->Draw();
+    //изменение внешнего вида бомбы
+    ScreenSingleton::getInstance().GotoXY(m_bomb->GetX(), m_bomb->GetY() - 1);
+    std::cout << "|";
+}
+
+void BombDecorator::SetPos(double nx, double ny)
+{
+    m_bomb->SetPos(nx, ny);
+}
+
+uint16_t BombDecorator::GetWidth() const
+{
+    return m_bomb->GetWidth();
 }
