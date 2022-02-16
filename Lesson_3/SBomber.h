bbfd184290261114deeecdc3c52d3c14ed958edf
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-02-16 23:05:00 +0300

    Update SBomber.h

--- Lesson_3/SBomber.h
+++ Lesson_3/SBomber.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <vector>
-
 #include "LevelGUI.h"
 #include "Plane.h"
 #include "Bomb.h"
@@ -11,34 +10,61 @@
 class SBomber
 {
 public:
-
     SBomber();
     ~SBomber();
     
-    inline bool GetExitFlag() const { return exitFlag; }
-
+    inline bool GetExitFlag() const 
+{ 
+return exitFlag; 
+}
     void ProcessKBHit();
     void TimeStart();
     void TimeFinish();
-
     void DrawFrame();
     void MoveObjects();
     void CheckObjects();
 
+class BombIterator
+    {
+
+ public:
+
+        BombIterator(std::vector<DynamicObject*>& dat);
+        void reset();
+
+        BombIterator& operator++();
+        BombIterator& operator--();
+        Bomb* operator* ();
+
+        bool operator== (BombIterator it);
+        bool operator!=(BombIterator it);
+
+        BombIterator& operator= (const BombIterator& it);
+        BombIterator begin();
+        BombIterator end();
+
+private:
+
+        int Index;
+        Bomb** target;
+        std::vector<DynamicObject*>& iterVector;
+  
+    };
+    
+    BombIterator erase(BombIterator it);
+   
 private:
 
     void CheckPlaneAndLevelGUI();
     void CheckBombsAndGround();
-    void  CheckDestoyableObjects(Bomb* pBomb);
-
-    void  DeleteDynamicObj(DynamicObject * pBomb);
-    void  DeleteStaticObj(GameObject* pObj);
-
+    void CheckDestoyableObjects(Bomb* pBomb);
+    void DeleteDynamicObj(DynamicObject * pBomb);
+    void DeleteStaticObj(GameObject* pObj);
     Ground * FindGround() const;
     Plane * FindPlane() const;
     LevelGUI * FindLevelGUI() const;
     std::vector<DestroyableGroundObject*> FindDestoyableGroundObjects() const;
-    std::vector<Bomb*> FindAllBombs() const;
+
 
     void DropBomb();
 
@@ -46,7 +72,6 @@ private:
     std::vector<GameObject*> vecStaticObj;
     
     bool exitFlag;
-
     uint64_t startTime, finishTime, passedTime;
     uint16_t bombsNumber, deltaTime, fps;
     int16_t score;
