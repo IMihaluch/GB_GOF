623c5b9a9dda1735bd4f9c23d8ee3c7963b31f78
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-02-24 18:15:03 +0300

    Update DynamicObject.h

--- DynamicObject.h
+++ DynamicObject.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <stdint.h>
-
 #include "GameObject.h"
+#include "Visitor.h"
 
-class DynamicObject : public GameObject 
+struct Direction
+{
+    double xD, yD;
+};
+
+class DynamicObject : public GameObject
 {
 public:
 
@@ -12,8 +17,18 @@ public:
 
     inline void SetSpeed(double sp) { speed = sp; }
     inline void SetDirection(double dx, double dy) { xDirction = dx; yDirection = dy; }
-    
+
     virtual void Move(uint16_t time) { x += xDirction * speed * time * 0.001; y += yDirection * speed * time * 0.001; };
+
+    virtual void __fastcall Accept(Visitor* v) = 0;
+    virtual double GetSpeed() const { return speed; };
+    virtual Direction GetDirection() const 
+    {
+        Direction d;
+        d.xD = xDirction;
+        d.yD = yDirection;
+        return d;
+    };
 
 protected:
 
