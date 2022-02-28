983f973fdb142c254f8f163da6e547fdf44b9c57
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-02-28 20:59:19 +0300

    Update Plane.cpp

--- Plane.cpp
+++ Plane.cpp
@@ -1,19 +1,65 @@
-
 #include <iostream>
-
 #include "Plane.h"
 #include "MyTools.h"
-#include "ScreenSingleton.h"
+using namespace std;
+using namespace MyTools;
 
 void Plane::Draw() const
 {
+    Color();
+    DrawBody();
+    DrawTail();
+    DrawWings();
+
+    /*   ScreenSingleton::getInstance().GotoXY(x, y);
+       cout << "=========>";
+       ScreenSingleton::getInstance().GotoXY(x - 2, y - 1);
+       cout << "===";
+       ScreenSingleton::getInstance().GotoXY(x + 3, y - 1);
+       cout << "\\\\\\\\";
+       ScreenSingleton::getInstance().GotoXY(x + 3, y + 1);
+       cout << "////";
+   */
+}
+void Plane::Color() const 
+{
     ScreenSingleton::getInstance().SetColor(CC_LightBlue);
+}
+void Plane::DrawBody() const
+{
     ScreenSingleton::getInstance().GotoXY(x, y);
-    std::cout << "=========>";
+    cout << "=========>";
+}
+void Plane::DrawTail() const
+{
     ScreenSingleton::getInstance().GotoXY(x - 2, y - 1);
-    std::cout << "===";
+    cout << "===";
+}
+void Plane::DrawWings() const
+{
     ScreenSingleton::getInstance().GotoXY(x + 3, y - 1);
-    std::cout << "\\\\\\\\";
+    cout << "\\\\\\\\";
     ScreenSingleton::getInstance().GotoXY(x + 3, y + 1);
-    std::cout << "////";
+    cout << "////";
+}
+void ColorPlane::Color() const 
+{
+    ScreenSingleton::getInstance().SetColor(CC_Brown);
+}
+void BigPlane::DrawBody() const 
+{
+    ScreenSingleton::getInstance().GotoXY(x, y);
+    cout << "===============>";
+}
+void BigPlane::DrawTail() const 
+{
+    ScreenSingleton::getInstance().GotoXY(x - 2, y - 1);
+    cout << "======";
+}
+void BigPlane::DrawWings() const 
+{
+    ScreenSingleton::getInstance().GotoXY(x + 3, y - 1);
+    cout << "\\\\\\\\\\\\\\";
+    ScreenSingleton::getInstance().GotoXY(x + 3, y + 1);
+    cout << "////////";
 }
