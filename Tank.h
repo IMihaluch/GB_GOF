ec74d24b1f063a941c556b33566fdb0211f268b5
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-02-28 18:44:01 +0300

    Update Tank.h

--- Tank.h
+++ Tank.h
@@ -1,21 +1,29 @@
 #pragma once
 
+#include <string>
 #include <stdint.h>
-
 #include "DestroyableGroundObject.h"
+#include "Mediator.h"
 
 class Tank : public DestroyableGroundObject
 {
 public:
-
-	bool  isInside(double x1, double x2) const override;
+	bool __fastcall isInside(double x1, double x2) const override;
 
 	inline uint16_t GetScore() const override { return score; }
 
 	void Draw() const override;
 
+	void send_mes();
+
+	void Draw() const override;
+	Tank();
+	~Tank();
+
 private:
 
+	Mediator& mediator;
 	const uint16_t score = 30;
+	std::string messages[4] = { "You'll regret this!", "You are in my sights!",
+	"Get out rusty can!", "Your bombs are powerless against my armor!" };
 };
-
