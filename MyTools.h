32bbe15316f58ef3b141a4950585aca167526833
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-03-02 22:47:42 +0300

    Update MyTools.h

--- MyTools.h
+++ MyTools.h
@@ -1,18 +1,60 @@
 #pragma once
 
+#include <stdint.h>
 #include <string>
-#include <cstring>
+#include <fstream>
 
-namespace MyTools {
+namespace MyTools
+{
 
-void OpenLogFile(const std::string& FN);
+    // Палитра цветов от 0 до 15
+    enum ConsoleColor
+    {
+        CC_Black = 0,
+        CC_Blue,
+        CC_Green,
+        CC_Cyan,
+        CC_Red,
+        CC_Magenta,
+        CC_Brown,
+        CC_LightGray,
+        CC_DarkGray,
+        CC_LightBlue,
+        CC_LightGreen,
+        CC_LightCyan,
+        CC_LightRed,
+        CC_LightMagenta,
+        CC_Yellow,
+        CC_White
+    };
+    //=============================================================================================
+    void ClrScr();
+    void __fastcall GotoXY(double x, double y);
+    uint16_t GetMaxX();
+    uint16_t GetMaxY();
+    void SetColor(ConsoleColor color);
 
-void CloseLogFile();
+    //=============================================================================================
 
-void WriteToLog(const std::string& str);
+    
+    class FileLogger 
+    {
+        void OpenLogFile(const std::string& FN);
 
-void WriteToLog(const std::string& str, int n);
+        std::ofstream logOut;
+        void CloseLogFile();
 
-void WriteToLog(const std::string& str, double d);
+    public:
+        void WriteToLog(const std::string& str);
 
-}; // namespace MyTools
+        void WriteToLog(const std::string& str, int n);
+
+        void WriteToLog(const std::string& str, double d);
+
+        FileLogger();
+        ~FileLogger();
+    };
+
+    //=============================================================================================
+
+};
