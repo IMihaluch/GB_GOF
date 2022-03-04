9dbb414cbabea0e9e45032aa425b1ee74408ea3e
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-03-04 21:08:22 +0300

    Update Tank.h

--- Tank.h
+++ Tank.h
@@ -7,12 +7,25 @@
 class Tank : public DestroyableGroundObject
 {
 public:
+	Tank() {}
+
+	Tank(const Tank& _tank)
+	{
+		x = _tank.x;
+		y = _tank.y;
+		width = _tank.width;
+	}
 
 	bool  isInside(double x1, double x2) const override;
 
 	inline uint16_t GetScore() const override { return score; }
 
 	void Draw() const override;
+
+	Tank* clone() const
+	{
+		return new Tank(*this);
+	}
 
 private:
 
