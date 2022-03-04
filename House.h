25d7bacdeea44a3fc1221e8d40f775f37ff18af4
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-03-04 20:28:26 +0300

    Update House.h

--- House.h
+++ House.h
@@ -3,18 +3,34 @@
 #include "DestroyableGroundObject.h"
 #include <cstdint>
 
-class House : public DestroyableGroundObject 
-{
-public:
-          bool isInside(double x1, double x2) const override;
 
-          inline uint16_t GetScore() const override 
-         {
-          return score;
-         }
+    class House : public DestroyableGroundObject
+    {
+    public:
+        
+        House() {}
 
-         void Draw() const override;
+        House(const House& _house)
+        {
+            x = _house.x;
+            y = _house.y;
+            width = _house.width;
+        }
 
-private:
-            const uint16_t score = 40;
-};
+        bool isInside(double x1, double x2) const override;
+
+        inline uint16_t GetScore() const override
+        {
+            return score;
+        }
+
+        void Draw() const override;
+
+        House* clone() const
+        {
+            return new House(*this);
+        }
+
+    private:
+        const uint16_t score = 40;
+    };
