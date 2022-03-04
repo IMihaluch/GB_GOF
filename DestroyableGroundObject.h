119a1c0547683540e6963adcd777f6f34c881105
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-03-04 19:56:00 +0300

    Update DestroyableGroundObject.h

--- DestroyableGroundObject.h
+++ DestroyableGroundObject.h
@@ -11,6 +11,8 @@ public:
     virtual bool  isInside(double x1, double x2) const = 0;
 
     virtual inline uint16_t GetScore() const = 0;
+    
+    virtual DestroyableGroundObject* clone() const = 0;
 
 protected:
 
